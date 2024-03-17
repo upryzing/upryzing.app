46ca986bc232a1dce9de26d1a9ecbb4c3522d17c
Amy <amy@amogus.cloud>
2024-03-17 11:34:27 -0400

    chore: reorganize stuff, add dependencies for svg files and others
    I've also started to make the tagline/slogan section, hence the spinny.svg file :3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
+import icon from "astro-icon"
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    integrations: [icon({
+        iconDir: "public/icons/"
+    })]
+});

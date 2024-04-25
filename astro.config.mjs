a35c7a27471d7d208f98e4e3146d70d1af0e4aad
Amy <amy@amogus.cloud>
2024-04-25 09:11:09 -0400

    chore: use vercel adapter for server-side rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from 'astro/config';
 import icon from "astro-icon";
-
 import sitemap from "@astrojs/sitemap";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://upryzing.app",
   integrations: [icon({
     iconDir: "public/icons/"
-  }), sitemap()]
+  }), sitemap()],
+  output: "server",
+  adapter: vercel()
 });

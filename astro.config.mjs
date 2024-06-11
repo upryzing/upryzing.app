39a3d9bec0e9b653746802ca325c9d441da78925
Amy <amy@amogus.cloud>
2024-06-11 09:54:42 -0400

    remove: unused vercel serverless adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,11 @@
 import { defineConfig } from 'astro/config';
 import icon from "astro-icon";
 import sitemap from "@astrojs/sitemap";
-
-import vercel from "@astrojs/vercel/serverless";
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://upryzing.app",
   integrations: [icon({
     iconDir: "public/icons/"
   }), sitemap()],
-  output: "server",
-  adapter: vercel()
+  output: "server"
 });
